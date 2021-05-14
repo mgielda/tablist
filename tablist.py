a082c7592d1098e117c0ad33229de9d967a99bd8
Michael Gielda <mgielda@antmicro.com>
2021-05-14 22:46:11 +0200

    Add fuzzy finding

--- tablist.py
+++ tablist.py
@@ -10,49 +10,56 @@ app = typer.Typer(help="A tab list app", add_completion=False)
 data = None
 
 @app.command()
+@app.command("ls")
 def list_sections():
     "list available sections"
     print('Sections:\n%s' % ('\n'.join(list(data.keys()))))
 
 @app.command()
-def list_tabs(section_name: str, pretty: bool = False):
+@app.command("lt")
+def list_tabs(section_name: str, just_urls: bool = False):
     "list tabs in section"
     try:
         tabs = data[section_name]
     except KeyError:
-        print(f"Section '{section_name}' not in the YAML file")
-        return
-    if pretty:
-        print(f'Tabs in section {section_name}:')
+        print(f"Section '{section_name}' not in the YAML file, trying fuzzy find")
+        tabs = fzf(section_name)
+
+    urls = list(tabs.values())
+    if not just_urls:
+        print(f'Tabs for "{section_name}":')
         for index, (key, value) in enumerate(tabs.items()):
             print(colored(index, attrs=['bold']), colored(key, 'yellow'), value)
     else:
-        for t in list(tabs.values()):
-            print(t)
+        for u in urls:
+            print(u)
 
 # TODO allow fuzzy finding of arguments / tab names in CLI mode
 # perhaps in a new CLI library?
 
 @app.command()
+@app.command("ot")
 def open_tabs(section_name: str, tab_no: Optional[int] = typer.Argument(None)):
     "open tabs in section in browser"
     try:
-        tabs = list(data[section_name].values())
+        tabs = data[section_name]
     except KeyError:
-        print(f"Section '{section_name}' not in the YAML file")
-        return
+        print(f"Section '{section_name}' not in the YAML file, trying fuzzy find.")
+        tabs = fzf(section_name)
+
+    urls = list(tabs.values())
     import subprocess
     if tab_no is not None:
-        if tab_no >= len(tabs):
-            print(f"Tab number {tab_no} not existent in '{section_name}' section")
+        if tab_no >= len(urls):
+            print(f"Number {tab_no} higher than number of tabs in '{section_name}'")
             return
-        process = subprocess.Popen(['xdg-open', tabs[tab_no]])
+        process = subprocess.Popen(['xdg-open', urls[tab_no]])
     else:
-        for t in tabs:
-            process = subprocess.Popen(['xdg-open', t])
+        for u in urls:
+            process = subprocess.Popen(['xdg-open', u])
 
 @app.callback(hidden=True)
-def tablist (file: str = "tabs.yml"):
+def tablist(file: str = "tabs.yml"):
     global data
     try:
         with open(file, 'r') as f:
@@ -61,6 +68,19 @@ def tablist (file: str = "tabs.yml"):
         import sys
         print(f"File {file} not found or is not a proper YAML file.")
         sys.exit(1)
+
+def fzf(search_string: str):
+    from fuzzyfinder import fuzzyfinder
+    keys = []
+    tabs = {}
+    for s in data:
+        for k in data[s]:
+            keys += [k]
+            tabs[k] = data[s][k]
+    suggestions = fuzzyfinder(search_string, keys)
+
+    filtered_data = { x: tabs[x] for x in suggestions }
+    return(filtered_data)
 
 import click
 from click_repl import repl
